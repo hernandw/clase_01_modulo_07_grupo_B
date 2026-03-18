716a9819b9b8d8bb784158cbca012ac338c23a92
Williams Hernández <hernandw@gmail.com>
2026-03-18 20:20:23 -0300

    :sparkles: configuracion de la BBDD

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,19 @@
 import pg from 'pg'
+import 'dotenv/config'
+
 
 const { Pool } = pg
+const {DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME} = process.env
+
+console.log(process.env)
 
 const config = {
-    user: '',
-    password: '',
-    database: '',
-    port: '',
-    host:''
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT,
+    host: DB_HOST
 }
+
+
+
